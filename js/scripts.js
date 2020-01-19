7f7d35eafc6d1f794b75086ecb5ee404a54e817b
Jiwon Han <jiwon1.han@gmail.com>
2020-01-18 17:15:29 -0800

    Half complete

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,49 @@
 
+$(document).ready(function() {
+  $("button#start").click(function(event){
+    $("form#language").show();
+  })
+})
 
 $(document).ready(function() {
-  $("form#delivery").submit(function(event) {
-
-    var flavor = $("input:radio[name=flavor]:checked").val();
+  $("form#language").submit(function(event) {
+    event.preventDefault();
+    var data = $("input:radio[name=data]:checked").val();
+    var duration = $("input:radio[name=duration]:checked").val();
     var result;
-    if (flavor === "shio") {
-      result = "Shio"
-    } else if (flavor === "tofu") {
-      result = "Tofu"
-    } else if (flavor === "spicyt") {
-      result = "Spicy Tonkotsu Ramen"
-    } else if (flavor === "shoyu") {
-      result = "Shoyu Ramen"
+    if (duration === "long") {
+      $("#result-long").show();
+      $("#result-intro").show();
+      $("#result-sql").show();
+      $("#result-python").show();
+      $("#result-javascript").show();
+    } else if (duration === "short" && data === "sql2") {
+      $("#result-intro").show();
+      $("#result-sql").show();
+      $("#result-python").hide();
+      $("#result-javascript").hide();
+    } else if (duration === "short" && data === "python2") {
+      $("#result-intro").show();
+      $("#result-sql").hide();
+      $("#result-python").show();
+      $("result-javascript").hide();
+    } else if (duration === "short" && data === "javascript2") {
+      $("#result-intro").show();
+      $("#result-sql").hide();
+      $("#result-python").hide();
+      $("#result-javascript").show();
+    } else if (duration === "short" && duration === "long") {
+      $("#result-intro").show();
     }
 
     var nameInput = $("input#name").val();
-    var addressInput = $("input#address").val();
-    var contactInput = $("input#contact").val();
+    $("#you").text(nameInput)
 
     $("#output").show();
     $("#ramen").text(result);
     $("#recipient").text(nameInput);
     $("#location").text(addressInput);
     $("#phone").text(contactInput);
-    event.preventDefault();
+
   });
 });
